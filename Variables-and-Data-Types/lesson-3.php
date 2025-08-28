f5bcd68299bab764768c3019bf8eb8e2d44efb76
Atharvaa99 <atharvadhaigude@icloud.com>
2025-08-28 20:03:37 +0530

    Commit

---
+++ Variables-and-Data-Types/lesson-3.php
@@ -0,0 +1,10 @@
+<?php
+// Read input
+$name = trim(fgets(STDIN));
+
+// TODO: Write your code here
+// Create the $greeting variable using double quotes and variable interpolation
+$greeting = "Welcome to PHP, $name!";
+// Output the greeting
+echo $greeting;
+?>
