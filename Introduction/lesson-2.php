8f738aa9fb35024b45893e3675caa119d709c015
Atharvaa99 <atharvadhaigude@icloud.com>
2025-08-28 19:39:17 +0530

    Commit

---
+++ Introduction/lesson-2.php
@@ -0,0 +1,15 @@
+<?php
+// This is a single-line comment
+echo "Hello World!";
+
+# This is also a single-line comment
+echo "PHP is great!";
+?>
+<?php
+/*
+This is a multi-line comment.
+It can span several lines and is useful
+for detailed explanations.
+*/
+echo "Learning PHP comments!";
+?>
---
+++ Introduction/lesson_3.php
@@ -0,0 +1,8 @@
+<?php
+// TODO: Write your code below to display the poem using four separate echo statements
+// Remember to use \n to create new lines between each line of the poem
+echo "Roses are red\n";
+echo "Violets are blue\n";
+echo "PHP is awesome\n";
+echo "And so are you!\n";
+?>
