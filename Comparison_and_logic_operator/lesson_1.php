8fb55419548d4aa28466eec5d111afb527c9f81a
Atharvaa99 <atharvadhaigude@icloud.com>
2025-09-03 18:29:37 +0530

    Commit

---
+++ Comparison_and_logic_operator/lesson_1.php
@@ -0,0 +1,7 @@
+<?php
+$username = "admin";
+$password = "secret";
+$validLogin = ($username == "admin") && ($password == "secret");
+// Don't change the line below
+echo "Login valid: " . ($validLogin ? "true" : "false");
+?>
