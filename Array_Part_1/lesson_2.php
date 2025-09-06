710311d1bc642f32da675ad5823219d8296c7acc
Atharvaa99 <atharvadhaigude@icloud.com>
2025-09-06 22:55:33 +0530

    Commit

---
+++ Array_Part_1/lesson_2.php
@@ -0,0 +1,13 @@
+<?php
+// Predefined colors array
+$colors = ["red", "blue", "green", "yellow", "purple"];
+
+// Read input
+$index = trim(fgets(STDIN));
+
+// TODO: Write your code here
+// Convert the input to integer and access the array element
+
+// Output the result
+echo "Color at index $index: $colors[$index]";
+?>
